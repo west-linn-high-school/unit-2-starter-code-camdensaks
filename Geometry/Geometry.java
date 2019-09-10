c79ac1a3895fbe84076ade7d0e58ef310f0f0fd2
Michael George <camdensaks@gmail.com>
2019-09-10 09:42:15 -0700

    Unit 2 - Jabberwocky and Geometry

---
+++ Geometry/Geometry.java
@@ -0,0 +1,21 @@
+package Geometry;
+
+
+/**
+ * Write a description of class Geometry here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Geometry
+{
+    public static void main(String[] args){
+        int sides = 7;
+        int radius = 5;
+        final double PI = 22/7;
+        System.out.println("A heptagon has " + sides + " sides.");
+        System.out.println("A Decagon has " + (sides + 3) + " sides.");
+        System.out.println("A Dodecagon has " + (sides + radius) + " sides.");
+        System.out.println("The circumference of a circle with radius " + radius + " is " + (2*PI*radius));
+    }
+}    
---
+++ Jabberwocky.java
@@ -0,0 +1,16 @@
+
+/**
+ * The first stanza of the Jabberwocky
+ *
+ * @author Camden
+ * @version (a version number or a date)
+ */
+public class Jabberwocky
+{
+    public static void main(String[] args){
+    System.out.print("'Twas brillig, and the slithy toves/\n");
+    System.out.print("Did gyre and gimble in the wabe:\n");
+    System.out.print("All mimsy were the \"borogroves\"/\n");
+    System.out.print("And the mome raths out grabe.");
+   }
+}
